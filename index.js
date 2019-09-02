38b28cb0db4c398dcc71ca67114e61ea731e1ef3
Olly Hayes <olly.hayes@gmail.com>
2019-09-02 17:38:39 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const {spawnSync} = require('child_process');
+const express = require('express');
+
+const findVolume = /\[(\d+)%\]/;
+function handleVolumeRequest(sign) {
+	return (req, res) => {
+		const result = spawnSync('amixer', ['set', 'Master', `2%${sign}`], {encoding: 'utf8'});
+		const volumeResult = findVolume.exec(result.stdout);
+		res.status(200).end(volumeResult[1]);
+	};
+}
+
+const expressApp = express();
+expressApp.post('/volume-up', handleVolumeRequest('+'));
+expressApp.post('/volume-down', handleVolumeRequest('-'));
+expressApp.listen(3000);
